c0ce24349cba084db76f892d900d0b432ce70242
kikext <e-imbert@hotmail.com>
2016-12-20 18:35:38 +0100

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Triangulo num1 = new Triangulo(3, 2);
+
+    }
+}
---
+++ src/com/company/Triangulo.java
@@ -0,0 +1,69 @@
+package com.company;
+
+/**
+ * Created by 25374183p on 20/12/2016.
+ */
+public class Triangulo {
+
+    private double lado;
+    private double base;
+
+
+
+    //------- CONSTRUCTOR -------------
+    public Triangulo(double lado, double base) {
+        this.lado = lado;
+        this.base = base;
+    }
+
+    //------- GETTERS ------------------
+
+    public double getLado() {
+        return lado;
+    }
+
+    public double getBase() {
+        return base;
+    }
+
+    //--------- SETTERS ------------
+
+
+    public void setLado(double lado) {
+        this.lado = lado;
+    }
+
+    public void setBase(double base) {
+        this.base = base;
+    }
+
+
+
+    public double calculaH(){
+        double a = Math.pow(lado,2);
+        double b = Math.pow((base/2),2);
+        double altura = Math.sqrt(a-b);
+        return altura;
+    }
+
+    public double calculaA(){
+        double Area = (base*calculaH()/2);
+        return Area;
+    }
+
+    public double perimetro(){
+        double perimetro = (2*lado)+base;
+        return perimetro;
+    }
+
+
+    //------------- toString ------------------
+
+    @Override
+    public String toString() {
+        return "Triangulo{" +
+                "lado=" + lado +
+                ", altura=" + base +
+                '}';
+    }
+}
